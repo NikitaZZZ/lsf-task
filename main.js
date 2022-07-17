3e401bf6052e1d71dc4789474e7a798e6af67da5
Nikita <zininnikita309@gmail.com>
2022-07-17 15:48:54 +0900

    prettier progon

--- main.js
+++ main.js
@@ -28,42 +28,36 @@ let app = new Vue({
             answersOptions: ['Da4', 'Net4', 'Da5', 'Net5'],
             correctAnswer: 'Net5',
           },
-        ]
+        ],
       },
 
       test2: {
         name: 'Test 2',
         description: 'This is test 2',
-        questions: [
-          '5?',
-          '6?',
-          '7?',
-          '8?',
-        ]
-      }
-    }
+        questions: ['5?', '6?', '7?', '8?'],
+      },
+    },
   },
 
   methods: {
-    nextQuestion (value) {
+    nextQuestion(value) {
       this.userAnswers.push(value);
       setTimeout(() => document.querySelector('form').reset(), 100);
 
-      if (this.currentQuestion+1 != this.tests.test1.questions.length) {
+      if (this.currentQuestion + 1 != this.tests.test1.questions.length) {
         this.currentQuestion += 1;
       } else {
-        this.end = true; 
+        this.end = true;
         this.endTest();
       }
     },
 
-    endTest () {
+    endTest() {
       for (let i = 0; i < this.tests.test1.questions.length; i++) {
-        console.log(this.userAnswers[i], this.tests.test1.questions[i].correctAnswer);
         if (this.userAnswers[i] == this.tests.test1.questions[i].correctAnswer) {
           this.correctResults += 1;
         }
       }
-    }
-  }
-})
+    },
+  },
+});
